a670fdcd143a5b551b415f609e6d470fb4c4a671
Michael Graham <michael@forested.earth>
2020-08-23 16:46:02 -0700

    graphql-node tutorial, step 3
    https://www.howtographql.com/graphql-js/3-a-simple-mutation/

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,38 +1,48 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
-
 let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
+    link: (parent, args) => {
+      const idx = links.findIndex(l => l.id === args.id)
+      return links[idx]
+    }
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (parent, args) => {
+      const idx = links.findIndex(l => l.id === args.id)
+      links[idx].description = args.description || links[idx].description
+      links[idx].url = args.url || links[idx].url
+      return links[idx]
+    },
+    deleteLink: (parent, args) => {
+      const idx = links.findIndex(l => l.id === args.id)
+      const link = {...links[idx]}
+      links.splice(idx, 1)
+      return link
+    }
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
