51d27c332f021b7964c12d1623e217dc07d83bb6
Michael Graham <michael@forested.earth>
2020-08-25 21:00:08 -0700

    graphql-node: authentication
    https://www.howtographql.com/graphql-js/6-authentication/

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,6 +1,19 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { PrismaClient } = require('@prisma/client')
 
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link
+}
+
+/*
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
@@ -36,14 +49,17 @@ const resolvers = {
     }
   }
 }
-
+*/
 const prisma = new PrismaClient()
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: {
-    prisma,
+  context: request => {
+    return {
+      ...request,
+      prisma
+    }
   }
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
