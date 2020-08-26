76d4df46c7a63e0e3cebe3ca1ee6f8a9737dbf22
Michael Graham <michael@forested.earth>
2020-08-25 18:34:17 -0700

    graphql-node: Connect server and database
    https://www.howtographql.com/graphql-js/5-connecting-server-and-database/

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,30 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    },
     link: (parent, args) => {
       const idx = links.findIndex(l => l.id === args.id)
       return links[idx]
     }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          description: args.description,
+          url: args.url,
+        }
+      })
+      return newLink
     },
     updateLink: (parent, args) => {
       const idx = links.findIndex(l => l.id === args.id)
@@ -41,8 +37,13 @@ const resolvers = {
   }
 }
 
+const prisma = new PrismaClient()
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: {
+    prisma,
+  }
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
