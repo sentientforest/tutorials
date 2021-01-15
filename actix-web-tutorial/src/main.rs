cd5025bf061f0ad77f426b2f01fecb9990d53c19
Michael Graham <michael@forested.earth>
2021-01-14 22:32:03 -0800

    actix-web: Shared Mutable State
    https://actix.rs/docs/application/

--- actix-web-tutorial/src/main.rs
+++ actix-web-tutorial/src/main.rs
@@ -1,24 +1,27 @@
-use actix_web::{get, web, App, HttpServer};
+use actix_web::{web, App, HttpServer};
+use std::sync::Mutex;
 
-struct AppState {
-    app_name: String,
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
 }
 
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
 
-    format!("Hello {}!", app_name)
+    format!("Request number: {}", counter)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .data(AppState {
-                app_name: String::from("Actix-web"),
-            })
-            .service(index)
+            .app_data(counter.clone())
+            .route("/", web::get().to(index))
     })
     .bind("127.0.0.1:8080")?
     .run()
